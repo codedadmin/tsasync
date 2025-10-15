476ec8022440a1d72e59622ca77134463c8ff837
Developer <mjat09@outlook.com>
2025-10-15 18:52:12 +0300

    mywork

--- index.ts
+++ index.ts
@@ -1,2 +1,11 @@
 import { getUser, getInput } from "./utilities";
 
+
+async function userInput() {
+        console.log("Please choose your drink:");
+        const input = await getInput();
+        console.log(input);
+}
+
+
+userInput();
