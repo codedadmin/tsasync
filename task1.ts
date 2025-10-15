1ccbaa6b42fe2d82d480cf0bfd054dfbaf4e5574
sals-lab <alomar.s95@gmail.com>
2025-10-15 20:00:23 +0300

    task 1 solved

--- task1.ts
+++ task1.ts
@@ -1,3 +1,8 @@
+import { privateDecrypt } from "crypto";
+import { get } from "http";
+import { getInput } from "./utilities";
+import { setDefaultAutoSelectFamilyAttemptTimeout } from "net";
+
 interface DrinkBakery {
   id: number;
   name: string;
@@ -13,3 +18,21 @@ const drinkBakeryItems: DrinkBakery[] = [
 ];
 
 // Take the user choice and quantity and show the total price
+// console.log("Menu");
+// drinkBakeryItems.forEach((item) => {
+//   console.log(`${item.id} - ${item.name} - $${item.price}`);
+// });
+
+async function userChoice() {
+  console.table(drinkBakeryItems);
+  const inputId = await getInput();
+  const selectedItem = drinkBakeryItems.find((drink: DrinkBakery) => {
+    if (drink.id.toString() == inputId) return true;
+  });
+  console.log(`how many you want sir`);
+  console.log(selectedItem.price);
+  const inputQuality = await getInput();
+  console.log(`Your total is ${selectedItem.price * parseInt(inputQuality)}`);
+}
+
+userChoice();
